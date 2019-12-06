23265225754fd201919df82ccb0bb1429264c64b
Shahid <shaahid.xd@gmail.com>
2019-12-06 20:01:10 +0530

    Change login and signup to functional components

--- src/modules/auth/screens/login/Login.js
+++ src/modules/auth/screens/login/Login.js
@@ -1,65 +1,68 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Formik, Field, ErrorMessage, Form } from 'formik';
 import { withRouter } from 'react-router-dom';
 
 import { login } from '../../actions/auth';
 
-class LoginScreen extends Component {
-  componentDidUpdate() {
-    if (this.props.auth.authDetails) {
-      this.props.history.replace('/');
-    }
-  }
-  handleLoginSubmit = ({ username, password }, { setSubmitting }) => {
-    this.props.loginUser({ username, password });
+const LoginScreen = props => {
+  const { isLoggingIn } = props.auth;
+
+  const handleLoginSubmit = ({ username, password }, { setSubmitting }) => {
+    props.loginUser({ username, password });
   };
-  render() {
-    const { isLoggingIn } = this.props.auth;
-    return (
-      <div>
-        <Formik
-          initialValues={{ username: '', password: '' }}
-          validate={values => {
-            const errors = {};
-            if (!values.username) {
-              errors.username = 'Username is required.';
-            }
-            if (!values.password) {
-              errors.password = 'Password is required.';
-            }
-            return errors;
-          }}
-          onSubmit={this.handleLoginSubmit}
-        >
-          {({ isSubmitting }) => {
-            return (
-              <Form
-                style={{
-                  border: '1px solid black',
-                  padding: '50px',
-                  width: '300px',
-                  display: 'flex',
-                  flexDirection: 'column'
-                }}
-              >
-                <label>Username</label>
-                <Field type="username" name="username" />
-                <ErrorMessage name="username" />
-                <label>Password</label>
-                <Field type="password" name="password" />
-                <ErrorMessage name="password" />
-                <button type="submit" disabled={isLoggingIn}>
-                  Submit
-                </button>
-              </Form>
-            );
-          }}
-        </Formik>
-      </div>
-    );
-  }
-}
+
+  useEffect(() => {
+    const { authDetails } = props.auth;
+
+    if (authDetails) {
+      props.history.replace('/');
+    }
+  });
+
+  return (
+    <div>
+      <Formik
+        initialValues={{ username: '', password: '' }}
+        validate={values => {
+          const errors = {};
+          if (!values.username) {
+            errors.username = 'Username is required.';
+          }
+          if (!values.password) {
+            errors.password = 'Password is required.';
+          }
+          return errors;
+        }}
+        onSubmit={handleLoginSubmit}
+      >
+        {({ isSubmitting }) => {
+          return (
+            <Form
+              style={{
+                border: '1px solid black',
+                padding: '50px',
+                width: '300px',
+                display: 'flex',
+                flexDirection: 'column'
+              }}
+            >
+              <label>Username</label>
+              <Field type="username" name="username" />
+              <ErrorMessage name="username" />
+              <label>Password</label>
+              <Field type="password" name="password" />
+              <ErrorMessage name="password" />
+              <button type="submit" disabled={isLoggingIn}>
+                Submit
+              </button>
+            </Form>
+          );
+        }}
+      </Formik>
+    </div>
+  );
+};
 
 const mapStateToProps = ({ auth }, ownProps) => {
   return {
--- src/modules/auth/screens/signup/Signup.js
+++ src/modules/auth/screens/signup/Signup.js
@@ -1,92 +1,93 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Formik, Field, ErrorMessage, Form } from 'formik';
 import { withRouter } from 'react-router-dom';
 
 import { signup } from '../../actions/auth';
 
-class SignupScreen extends Component {
-  componentDidUpdate() {
-    if (this.props.auth.authDetails) {
-      this.props.history.replace('/');
-    }
-  }
-  handleSignupSubmit = (values, { setSubmitting }) => {
+const SignupScreen = props => {
+  const handleSignupSubmit = (values, { setSubmitting }) => {
     const { username, email, password } = values;
 
-    this.props.signupUser({ username, email, password });
+    props.signupUser({ username, email, password });
   };
 
-  render() {
-    const { isSigningUp } = this.props.auth;
-    return (
-      <div>
-        <Formik
-          initialValues={{
-            username: '',
-            password: '',
-            email: '',
-            confirmPassword: ''
-          }}
-          validate={values => {
-            const { username, password, email, confirmPassword } = values;
-            const errors = {};
-            if (!username) {
-              errors.username = 'Username is required.';
-            }
-            if (!password) {
-              errors.password = 'Password is required.';
-            }
-            if (!email) {
-              errors.email = 'Email is required';
-            }
-            if (!confirmPassword) {
-              errors.confirmPassword = 'Password confirmation is required.';
-            } else if (confirmPassword !== password) {
-              errors.confirmPassword = 'Passwords should match.';
-            }
-            return errors;
-          }}
-          onSubmit={this.handleSignupSubmit}
-        >
-          {({ isSubmitting }) => {
-            return (
-              <Form
-                style={{
-                  border: '1px solid black',
-                  padding: '50px',
-                  width: '300px',
-                  display: 'flex',
-                  flexDirection: 'column'
-                }}
-              >
-                <label>Username</label>
-                <Field type="username" name="username" />
-                <ErrorMessage name="username" />
+  useEffect(() => {
+    const { authDetails } = props.auth;
 
-                <label>Email</label>
-                <Field type="email" name="email" />
-                <ErrorMessage name="email" />
+    if (authDetails) {
+      props.history.replace('/');
+    }
+  });
 
-                <label>Password</label>
-                <Field type="password" name="password" />
-                <ErrorMessage name="password" />
+  const { isSigningUp } = props.auth;
+  return (
+    <div>
+      <Formik
+        initialValues={{
+          username: '',
+          password: '',
+          email: '',
+          confirmPassword: ''
+        }}
+        validate={values => {
+          const { username, password, email, confirmPassword } = values;
+          const errors = {};
+          if (!username) {
+            errors.username = 'Username is required.';
+          }
+          if (!password) {
+            errors.password = 'Password is required.';
+          }
+          if (!email) {
+            errors.email = 'Email is required';
+          }
+          if (!confirmPassword) {
+            errors.confirmPassword = 'Password confirmation is required.';
+          } else if (confirmPassword !== password) {
+            errors.confirmPassword = 'Passwords should match.';
+          }
+          return errors;
+        }}
+        onSubmit={handleSignupSubmit}
+      >
+        {({ isSubmitting }) => {
+          return (
+            <Form
+              style={{
+                border: '1px solid black',
+                padding: '50px',
+                width: '300px',
+                display: 'flex',
+                flexDirection: 'column'
+              }}
+            >
+              <label>Username</label>
+              <Field type="username" name="username" />
+              <ErrorMessage name="username" />
 
-                <label>Confirm Password</label>
-                <Field type="password" name="confirmPassword" />
-                <ErrorMessage name="confirmPassword" />
+              <label>Email</label>
+              <Field type="email" name="email" />
+              <ErrorMessage name="email" />
 
-                <button type="submit" disabled={isSigningUp}>
-                  Submit
-                </button>
-              </Form>
-            );
-          }}
-        </Formik>
-      </div>
-    );
-  }
-}
+              <label>Password</label>
+              <Field type="password" name="password" />
+              <ErrorMessage name="password" />
+
+              <label>Confirm Password</label>
+              <Field type="password" name="confirmPassword" />
+              <ErrorMessage name="confirmPassword" />
+
+              <button type="submit" disabled={isSigningUp}>
+                Submit
+              </button>
+            </Form>
+          );
+        }}
+      </Formik>
+    </div>
+  );
+};
 
 const mapStateToProps = ({ auth }, ownProps) => {
   return {
