27a0550c8e14fb76c9f6054b94fbf829247c6718
Shahid <shaahid.xd@gmail.com>
2019-12-17 09:04:54 +0530

    Display vehicle in vehicle screen

--- src/modules/screens/vehicle/Vehicle.js
+++ src/modules/screens/vehicle/Vehicle.js
@@ -1,10 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import DatePicker from 'react-datepicker';
+
+import 'react-datepicker/dist/react-datepicker.css';
 
 import { get_vehicle } from '../../actions/vehicles';
 
+import './Vehicle.css';
+import { getImageUrl } from '../../helper/vehicleHelper';
+
 const VehicleScreen = props => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [returnDate, setReturnDate] = useState(new Date());
+
   useEffect(
     () => {
       props.getVehicle(props.match.params.id);
@@ -17,6 +26,18 @@ const VehicleScreen = props => {
     getVehicleLoading,
     getVehicleError
   } = props.vehicles;
+
+  const handleStartDateChange = selectedDate => {
+    setStartDate(selectedDate);
+  };
+
+  const handleReturnDateChange = selectedDate => {
+    setReturnDate(selectedDate);
+  };
+
+  const handleBookClick = () => {
+    console.log('Booked');
+  };
 
   const renderLoading = () => {
     return (
@@ -35,9 +56,31 @@ const VehicleScreen = props => {
   };
 
   const renderVehicle = () => {
+    const { image, name } = selectedVehicle;
+
     return (
-      <div>
-        <h1>{JSON.stringify(selectedVehicle)}</h1>
+      <div className="vehicle__display-div">
+        <img src={getImageUrl(image)} alt="Sample" className="vehicle__image" />
+        <h1 className="vehicle__label-header">{name}</h1>
+        <DatePicker
+          showPopperArrow={false}
+          selected={startDate}
+          onChange={handleStartDateChange}
+          className="vehicle__datepicker"
+        />
+        <DatePicker
+          showPopperArrow={false}
+          selected={returnDate}
+          onChange={handleReturnDateChange}
+          className="vehicle__datepicker"
+        />
+        <button
+          type="button"
+          onClick={handleBookClick}
+          className="vehicle__button-book"
+        >
+          Book Now!
+        </button>
       </div>
     );
   };
