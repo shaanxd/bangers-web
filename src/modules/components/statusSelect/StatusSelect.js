2661730403911f3bbed8df4b67fe51379b58cc45
Shahid <shaahid.xd@gmail.com>
2020-02-18 23:12:57 +0530

    Update admin home

--- src/modules/components/statusSelect/StatusSelect.js
+++ src/modules/components/statusSelect/StatusSelect.js
@@ -1,10 +1,38 @@
 import React from 'react';
 
-import styles from './StatusSelect.module.css';
 import { BOOKING_STATUS } from '../../constants/constants';
+import { formatDateFromUtc } from '../../helper/vehicleHelper';
+
+import styles from './StatusSelect.module.css';
 
 const StatusSelect = props => {
-  const { onClick, value } = props;
+  const {
+    onClick,
+    value,
+    booking: {
+      id,
+      vehicle: { name },
+      startDate,
+      returnDate,
+      bookingStatus,
+      equipment
+    }
+  } = props;
+
+  const renderEquipmentList = () => {
+    if (equipment.length === 0) {
+      return 'None';
+    }
+    let val = '';
+    for (let i = 0; i < equipment.length; i++) {
+      if (i === equipment.length - 1) {
+        val += `${equipment[i].name}`;
+      } else {
+        val += `${equipment[i].name}, `;
+      }
+    }
+    return val;
+  };
 
   const renderOptions = () => {
     const components = Object.keys(BOOKING_STATUS).map(option => {
@@ -24,7 +52,37 @@ const StatusSelect = props => {
     });
     return components;
   };
-  return <div className={styles.main__div}>{renderOptions()}</div>;
+  return (
+    <div className={styles.main__div}>
+      <span className={styles.section__header}>BOOKING DETAILS</span>
+      <div className={styles.header__div}>
+        <div className={styles.header__light}>BOOKING</div>
+        <span className={styles.header__title}>{id.replace(/-/g, '').toUpperCase()}</span>
+      </div>
+      <div className={styles.header__div}>
+        <div className={styles.header__light}>VEHICLE</div>
+        <span className={styles.header__title}>{name}</span>
+      </div>
+      <div className={styles.header__div}>
+        <div className={styles.header__light}>START DATE</div>
+        <span className={styles.header__title}>{formatDateFromUtc(startDate)}</span>
+      </div>
+      <div className={styles.header__div}>
+        <div className={styles.header__light}>RETURN DATE</div>
+        <span className={styles.header__title}>{formatDateFromUtc(returnDate)}</span>
+      </div>
+      <div className={styles.header__div}>
+        <div className={styles.header__light}>STATUS</div>
+        <span className={styles.header__title}>{bookingStatus}</span>
+      </div>
+      <div className={styles.header__div}>
+        <div className={styles.header__light}>EQUIPMENT</div>
+        <span className={styles.header__title}>{renderEquipmentList()}</span>
+      </div>
+      <span className={styles.section__header}>UPDATE STATUS</span>
+      <div className={styles.list__div}>{renderOptions()}</div>
+    </div>
+  );
 };
 
 export default StatusSelect;
--- src/modules/screens/adminHome/AdminHome.js
+++ src/modules/screens/adminHome/AdminHome.js
@@ -1,27 +1,25 @@
 import React, { useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import Moment from 'moment';
+import { IoIosClose } from 'react-icons/io';
+import { AiOutlineBook } from 'react-icons/ai';
 
-import { Loading, StatusSelect } from '../../components';
-import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io';
-import { Collapse } from 'react-collapse';
+import { Loading, StatusSelect, PageHeader, BookingItem, Glitch } from '../../components';
 import { useMergedState } from '../../helper/useMergedState';
 import { getBookings, updateBooking } from '../../api/admin';
 
 import styles from './AdminHome.module.css';
-import './AdminHome.css';
 
 const AdminHome = props => {
   const [state, setState] = useMergedState({
     bookingsLoading: true,
     bookings: [],
     bookingsError: null,
-    selectedId: null
+    selectedBooking: null
   });
 
   const { token } = props;
-  const { bookingsLoading, bookings, bookingsError, selectedId } = state;
+  const { bookingsLoading, bookings, bookingsError, selectedBooking } = state;
 
   useEffect(() => {
     loadBookingsFromApi();
@@ -31,12 +29,12 @@ const AdminHome = props => {
   const loadBookingsFromApi = async () => {
     try {
       if (!bookingsLoading) {
-        setState({ bookingsLoading: true, bookingsError: null });
+        setState({ bookingsLoading: true, bookingsError: null, selectedBooking: null });
       }
       const result = await getBookings(token);
       setState({ bookingsLoading: false, bookings: [...result.bookings] });
     } catch (err) {
-      setState({ bookingsLoading: false, bookingsError: err });
+      setState({ bookingsLoading: false, bookingsError: err.message });
     }
   };
 
@@ -46,8 +44,7 @@ const AdminHome = props => {
       await updateBooking(bookingData, token);
       loadBookingsFromApi();
     } catch (err) {
-      console.log(err.message);
-      setState({ bookingsLoading: false });
+      setState({ bookingsLoading: false, bookingsError: err.message });
     }
   };
 
@@ -56,67 +53,34 @@ const AdminHome = props => {
   };
 
   const renderEmpty = () => {
-    return <div>Empty</div>;
+    return (
+      <div className={styles.empty__div}>
+        <AiOutlineBook size={30} />
+        <span> No bookings found.</span>
+      </div>
+    );
   };
 
   const renderError = () => {
-    return <div>{bookingsError}</div>;
+    return <Glitch text={bookingsError} onRetry={loadBookingsFromApi} />;
   };
 
-  const onBookingSelect = id => {
-    setState({ selectedId: selectedId === id ? null : id });
+  const onBookingSelect = booking => {
+    setState({ selectedBooking: { ...booking } });
   };
 
-  const renderBookingItems = () => {
-    const items = bookings.map(booking => {
-      const { vehicle, id, user, startDate, returnDate, bookingStatus } = booking;
-      return (
-        <div className={styles.booking__root} key={id}>
-          <div
-            className={styles.booking__item}
-            onClick={() => {
-              onBookingSelect(id);
-            }}
-          >
-            <div className={styles.booking__content}>
-              <span className={styles.header__title}>BOOKED BY:</span>
-              <span className={styles.booking__id}>{`${user.firstname} ${user.lastname}`}</span>
-            </div>
-            <div className={styles.separator} />
-            <div className={styles.booking__content}>
-              <span className={styles.header__title}>VEHICLE</span>
-              <span className={styles.booking__id}>{vehicle.name}</span>
-            </div>
-            <div className={styles.separator} />
-            <div className={styles.booking__content}>
-              <span className={styles.header__title}>PICKED UP ON:</span>
-              <span className={styles.booking__id}>{Moment(startDate).format('dddd, MMMM Do YYYY')}</span>
-            </div>
-            <div className={styles.separator} />
-            <div className={styles.booking__content}>
-              <span className={styles.header__title}>RETURNED ON:</span>
-              <span className={styles.booking__id}>{Moment(returnDate).format('dddd, MMMM Do YYYY')}</span>
-            </div>
-            <button className={styles.button__container}>
-              {selectedId === id ? <IoIosArrowUp size={20} /> : <IoIosArrowDown size={20} />}
-            </button>
-          </div>
-          <Collapse isOpened={selectedId === id}>
-            <div className={styles.collapse__div}>
-              <span className={styles.header__title}>BOOKING STATUS:</span>
-              <StatusSelect
-                value={bookingStatus}
-                onClick={value => {
-                  onStatusChangeSubmit({ status: value, id });
-                }}
-              />
-            </div>
-          </Collapse>
-        </div>
-      );
+  const onBookingDeSelect = () => {
+    if (!bookingsLoading) {
+      setState({ selectedBooking: null });
+    }
+  };
+
+  const renderBookingList = () => {
+    const components = bookings.map(booking => {
+      return <BookingItem item={booking} key={booking.id} onSelect={onBookingSelect} />;
     });
 
-    return items;
+    return components;
   };
 
   return bookingsLoading ? (
@@ -127,7 +91,32 @@ const AdminHome = props => {
     renderEmpty()
   ) : (
     <div className={styles.main__div}>
-      <div className={styles.bookings__div}>{renderBookingItems()}</div>
+      <div className={styles.booking__list}>
+        <PageHeader text="ALL BOOKINGS" />
+        {renderBookingList()}
+      </div>
+      {selectedBooking && (
+        <div className={styles.modal__div}>
+          <div className={styles.backdrop__div} onClick={onBookingDeSelect}></div>
+          <div className={styles.side__drawer}>
+            <div className={styles.close__div}>
+              <button type="button" className={styles.close__button} onClick={onBookingDeSelect}>
+                <IoIosClose size={20} />
+                <span>CLOSE</span>
+              </button>
+            </div>
+            <div className={styles.flex__div} />
+            <StatusSelect
+              value={selectedBooking.bookingStatus}
+              onClick={value => {
+                onStatusChangeSubmit({ status: value, id: selectedBooking.id });
+              }}
+              booking={selectedBooking}
+            />
+            <div className={styles.flex__div} />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
